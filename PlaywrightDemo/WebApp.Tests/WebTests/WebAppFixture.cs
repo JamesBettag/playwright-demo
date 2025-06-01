3289ae21868e94e514403db5247a359c2842b494
JamesBettag <jbettag@tnsk.com>
2025-06-01 15:00:55 -0500

    get unit test working by upgrading project to net 9. created and passed title unit test

---
+++ PlaywrightDemo/WebApp.Tests/WebTests/WebAppFixture.cs
@@ -0,0 +1,27 @@
+﻿using Aspire.Hosting.Testing;
+using Aspire.Hosting;
+using Xunit;
+
+namespace WebApp.Tests.WebTests
+{
+	public class WebAppFixture : IAsyncLifetime
+	{
+		public Uri Endpoint { get; private set; }
+		private DistributedApplication? _app;
+
+		public async Task InitializeAsync()
+		{
+			var builder = await DistributedApplicationTestingBuilder.CreateAsync<Projects.PlaywrightDemo_AppHost>();
+			_app = await builder.BuildAsync();
+
+			await _app.StartAsync();
+
+			Endpoint = _app.GetEndpoint("webfrontend");
+		}
+
+		public async Task DisposeAsync()
+		{
+			await (_app?.DisposeAsync() ?? ValueTask.CompletedTask);
+		}
+	}
+}
---
+++ PlaywrightDemo/WebApp.Tests/WebTests/WebTests.cs
@@ -0,0 +1,22 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+using Xunit;
+
+namespace WebApp.Tests.WebTests;
+
+public class WebTests(WebAppFixture webAppFixture) : PageTest, IClassFixture<WebAppFixture>
+{
+	private readonly TimeSpan _defaultTimeout = TimeSpan.FromSeconds(15);
+
+    [Fact]
+    public async Task HasTitle()
+    {
+        // Use Playwright to navigate to the running web app
+        await Page.GotoAsync(webAppFixture.Endpoint.ToString());
+
+        await Expect(Page).ToHaveTitleAsync("Home");
+    }
+}
